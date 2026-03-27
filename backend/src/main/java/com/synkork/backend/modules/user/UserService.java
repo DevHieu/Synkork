4cac2d4c2a53e7a3412507466137e0048f86a370
DevHieu <hieudd2090@gmail.com>
2026-03-27 23:24:49 +0700

    Thêm chức năng quản lý phòng: hỗ trợ tạo phòng, tham gia phòng qua mã mời, và lấy mã mời hiện tại. Cập nhật các DTO và thực thể liên quan.

--- backend/src/main/java/com/synkork/backend/modules/user/UserService.java
+++ backend/src/main/java/com/synkork/backend/modules/user/UserService.java
@@ -1,12 +1,12 @@
 package com.synkork.backend.modules.user;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.synkork.backend.modules.user.dto.UserInfoDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 // Service là nơi xử lý các nghiệp vụ chính của ứng dụng
 // Nó sẽ tương tác với các repository để lấy dữ liệu từ database và thực hiện các logic nghiệp vụ
@@ -15,33 +15,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-  // @Autowired sẽ giúp Spring tự động tiêm các bean tương ứng vào, đỡ phải ghi
-  // code khởi tạo thủ công
-  @Autowired
-  UserRepository userRepository;
+    // @Autowired sẽ giúp Spring tự động tiêm các bean tương ứng vào, đỡ phải ghi
+    // code khởi tạo thủ công
+    @Autowired
+    UserRepository userRepository;
 
-  public List<UserEntity> findAll() {
-    return userRepository.findAll();
-  }
+    public List<UserEntity> findAll() {
+        return userRepository.findAll();
+    }
 
-  // @NonNull annotation giúp đảm bảo rằng user không được null, đỡ bị IDE báo
-  public UserEntity create(@NonNull UserEntity user) {
-    return userRepository.save(user);
-  }
+    public UserEntity findById(UUID userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
 
-  public UserInfoDto getUserInfo(String username) {
-      UserEntity user = userRepository.findByEmail(username)
-              .orElseGet(() -> userRepository.findByUsername(username)
-                      .orElse(null));
+    // @NonNull annotation giúp đảm bảo rằng user không được null, đỡ bị IDE báo
+    public UserEntity create(@NonNull UserEntity user) {
+        return userRepository.save(user);
+    }
 
-    return new UserInfoDto(user);
-  }
+    public UserInfoDto getUserInfo(String username) {
+        UserEntity user = userRepository.findByEmail(username)
+                .orElseGet(() -> userRepository.findByUsername(username)
+                        .orElse(null));
+
+        return new UserInfoDto(user);
+    }
 
     public UserEntity findByEmail(String email) {
-      return userRepository.findByEmail(email).orElse(null);
+        return userRepository.findByEmail(email).orElse(null);
     }
 
     public UserEntity updateUser(UserEntity existedUser) {
-      return userRepository.save(existedUser);
+        return userRepository.save(existedUser);
     }
+
 }
