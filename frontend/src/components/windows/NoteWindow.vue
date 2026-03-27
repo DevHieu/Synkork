d9e241b6135c4fcb5f57b7507eba8d3e3842ea1e
backend <huynhhieu210106@gmail.com>
2026-03-27 10:35:19 +0700

    commit truoc de pull

--- frontend/src/components/windows/NoteWindow.vue
+++ frontend/src/components/windows/NoteWindow.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const roomId = route.params.roomId;
+const spaceId = route.params.spaceId;
+
+</script>
 
 <template>
-  <div><h1>Note</h1></div>
+  <div><h1>{{ roomId }}</h1></div>
+  <div><h1>{{ spaceId }}</h1></div>
 </template>
 
 <style scoped></style>
