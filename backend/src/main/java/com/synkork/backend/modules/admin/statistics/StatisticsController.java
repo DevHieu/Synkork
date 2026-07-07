7bbfb51038a179fcf23531e36a0eb8412e4e21f2
thuyvy247 <thuyvy25012006@gmail.com>
2026-07-07 17:14:38 +0700

    Thêm top-reason chart

--- backend/src/main/java/com/synkork/backend/modules/admin/statistics/StatisticsController.java
+++ backend/src/main/java/com/synkork/backend/modules/admin/statistics/StatisticsController.java
@@ -4,9 +4,11 @@ import com.synkork.backend.modules.admin.statistics.dtos.OverviewChartResponse;
 import com.synkork.backend.modules.admin.statistics.dtos.OverviewStatsResponse;
 import com.synkork.backend.modules.admin.statistics.dtos.SubscriptionDashboardResponse;
 import com.synkork.backend.modules.admin.statistics.dtos.ReportChartResponse;
+import com.synkork.backend.modules.admin.statistics.dtos.ReportReasonStatsResponse;
 import com.synkork.backend.modules.admin.statistics.dtos.ReportStatsResponse;
 import com.synkork.backend.modules.admin.statistics.dtos.UserStatsResponse;
 import com.synkork.backend.modules.admin.statistics.enums.PeriodEnum;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,6 +58,11 @@ public class StatisticsController {
     ) {
         PeriodEnum periodEnum = PeriodEnum.valueOf(period);
         return ResponseEntity.ok(statisticsService.getReportChart(periodEnum));
+    }
+
+    @GetMapping("/dashboard/reports/top-reasons")
+    public ResponseEntity<List<ReportReasonStatsResponse>> getReportReasonStats() {
+        return ResponseEntity.ok(statisticsService.getReportReasonStats());
     }
 }
 
