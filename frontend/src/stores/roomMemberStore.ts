c8eecb8aaa9f0cf2e53315cc2a22274be5a40ca2
Phương Trâm <phuongtram300606@gmail.com>
2026-07-11 20:45:16 +0700

    Lưu thay đổi tạm

--- frontend/src/stores/roomMemberStore.ts
+++ frontend/src/stores/roomMemberStore.ts
@@ -39,6 +39,11 @@ export const useRoomMemberStore = defineStore("roomMember", {
         this.members.push(member);
       });
 
+      roomMemberSocket.subscribeMemberUpdated(roomId, (member) => {
+        this.updateMember(member);
+        this.setInfo(username);
+      });
+
       userSocket.subscribeKicked();
       userSocket.subscribeRoomDeleted();
     },
@@ -75,6 +80,12 @@ export const useRoomMemberStore = defineStore("roomMember", {
       this.currentAuthority = null;
     },
 
+    updateMember(member: Member) {
+      const idx = this.members.findIndex((m) => m.memberId === member.memberId);
+      if (idx !== -1) {
+        this.members[idx] = member;
+      }
+    },
 
   },
 
